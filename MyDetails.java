509fc4d5d9fcd49ca27b379d665b42a8bcc493bf
KatwalAkip <akip25.539@gmail.com>
2026-07-15 21:10:46 +0530

    first commit

---
+++ MyDetails.java
@@ -0,0 +1,8 @@
+public class MyDetails{
+    public static void main(String[] args) {
+        System.out.println("**My details**");
+        System.out.println("Name=k.akip");
+        System.out.println(20);
+        System.out.println("college=SSITS");
+    }
+}
