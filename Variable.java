fee55748e19ba39c40d395879a816be6501632b2
KatwalAkip <akip25.539@gmail.com>
2026-07-16 20:48:34 +0530

    using vatiable

---
+++ Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String[]args){
+        String name="katwal Akip";
+        int age=19;
+        System.out.println("my name is "+name);
+        System.out.println("i am "+age+" years old");
+        System.out.println("after one year i will be "+(age+1)+" years old.");
+    }
+    
+}
