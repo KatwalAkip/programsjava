4a8a380787fdc75d94a5c06972bbe8718c293dd4
KatwalAkip <akip25.539@gmail.com>
2026-07-16 20:56:48 +0530

    using data types

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        String name = "Karwal Akip";       // text
+        int age = 20;                // whole number
+        double height = 5.8;         // decimal number
+        char grade = 'A';            // one character
+        boolean isLearningJava = true; // true or false
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(grade);
+        System.out.println(isLearningJava);
+    }
+}
